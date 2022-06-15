793feb86fc0fd55a7651a9748b91de48b11bfe62
Goran Trajanvoic <goran.trajanovic09@gmail.com>
2022-06-15 17:09:55 +0200

    Fetching contenful data. #6

--- pages/library/index.js
+++ pages/library/index.js
@@ -75,7 +75,7 @@ export default function Library({ items }) {
 			});
 			setFeaturedItemsArray(tempItemsArray);
 		}
-	}, []);
+	}, [fetchedItems]);
 
 	// useEffect(() => {
 	// 	if (searchValue === "") {
